39a6f267a5867dc12fed2d7d15ea69b4e0bf0760
Lab-Chiesa <40397391+Lab-Chiesa@users.noreply.github.com>
2018-06-20 14:24:31 +0200

    Create run_random_merged_diets_analysis.sh

---
+++ run_random_merged_diets_analysis.sh
@@ -0,0 +1,59 @@
+#!bin/bash
+
+echo
+echo "========================================================================="
+echo "This simple script performs all steps needed to perform all correlations"
+echo "   in both miRNA and lipid datasets and reproduce the results of the"
+echo "   paper. Please note that, since the picking of values in the lipid"
+echo "  dataframe and their pairing with miRNA values can vary from analysis"
+echo "    to analysis (see the paper), actual results might slightly vary."
+echo "========================================================================="
+echo
+echo REQUIREMENTS
+echo ============
+echo
+echo Please note that Python 3.6+ is required to run supplied Python programs.
+echo There are also required dependencies to be installed, see each
+echo file requirements and/or see ModuleNotFoundError complaints.
+echo
+echo "** If in trouble with dependencies, try: less ./lib/manzutils.py **"
+echo
+echo MAKING & ANALYZING RANDOMIZED DATASETS
+echo =======================================
+echo
+echo In the paper, one thousand datasets (both miRNAs and lipids) have been
+echo randomized (see paper), and tested for correlation. This script
+echo produces one random dataset for both miRNAs and lipids, then performs
+echo the merged diets analysis on it.
+echo The script can be easily modified to produce as many random dataset
+echo as required, as well as to perform as many analyses as needed.
+echo Please consider that one full run produces 15-20 MB of data, and takes
+echo a few hours to complete.
+echo.
+echo Now running tests. Please note that this might take a few hours.
+
+export PYTHONPATH=$PYTHONPATH:./lib/
+
+# making the random datasets to analyze
+python ./source/dataset_randomizer.py -m data/miRNAs_dataset.csv -l data/lipidomics_dataset.csv
+
+# correlating miRNAs with lipids (default options, but merging diets)
+python ./reconciler/reconciler.py data/miRNAs_dataset.csv data/lipidomics_dataset.csv -d
+
+# changing the name of resulting csv table into something more apt
+mv statfriendly_mergediets_spearman_miRNAs_dataset_lipidomics_dataset.csv_0.01_0.7_SD_150p_allowed0aves_1.csv merged_diets_correlations.csv
+echo
+echo "** The correlations can be found in: merged_diets_correlations.csv **"
+echo
+
+# after calculating all correlations, we gather data in a final report.
+python ./source/final_reporter.py merged_diets_correlations.csv
+echo
+echo "**Open correlations_miR_enrich.csv to see the number of miRNAs passing tests.**"
+echo
+
+python ./source/mirna_heatmap_plotter.py merged_diets_correlations.csv -x -y -a -r -t 350 -f 0.7 -R
+echo
+echo See how tests cluster by opening merged_diets_correlations_mir_heatmap_t350.pdf
+echo
+echo Done!
