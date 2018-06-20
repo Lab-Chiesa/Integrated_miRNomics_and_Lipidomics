b0d54130e67217d49f17b13a7d0b6d7220280961
Lab-Chiesa <40397391+Lab-Chiesa@users.noreply.github.com>
2018-06-20 11:25:18 +0200

    Update run_merged_diets_analysis.sh

--- run_merged_diets_analysis.sh
+++ run_merged_diets_analysis.sh
@@ -15,8 +15,10 @@ echo
 echo Please note that Python 3.6+ is required to run Python programs.
 echo There are also required dependencies to be installed, see each
 echo file requirements and/or see ModuleNotFoundError complaints.
-echo If in trouble, try: less /lib/manzutils.py
 echo
+echo "   ** If in trouble with dependencies, try: less ./lib/manzutils.py **"
+echo
+echo Now running tests. Please note that this might take a few hours.
 
 export PYTHONPATH=$PYTHONPATH:./lib/
 
@@ -24,11 +26,11 @@ export PYTHONPATH=$PYTHONPATH:./lib/
 python ./reconciler/reconciler.py data/miRNAs_dataset.csv data/lipidomics_dataset.csv -d
 
 # changing the name of resulting csv table into something more apt
-mv statfriendly_mergediets_spearman_mirnas_anon_lipids_anon_0.01_0.7_SD_150p_allowed0aves_1.csv correlations.csv
-echo The correlations can be found in: correlations.csv
+mv statfriendly_mergediets_spearman_miRNAs_dataset_lipidomics_dataset.csv_0.01_0.7_SD_150p_allowed0aves_1.csv merged_diets_correlations.csv
+echo The correlations can be found in: merged_diets_correlations.csv
 
 # after calculating all correlations, we gather data in a final report.
-python final_reporter.py correlations.csv
+python final_reporter.py merged_diets_correlations
 echo "Open correlations_miR_enrich.csv to see the number of miRNAs passing tests."
 
 python mirna_heatmap_plotter.py correlations.csv -x -y -a -r -t 350 -f 0.7 -R
